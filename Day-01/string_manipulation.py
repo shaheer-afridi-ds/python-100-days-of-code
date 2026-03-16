74c210ff5f9f962da208324221888b465ea7649d
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-17 03:03:04 +0500

    Update string_manipulation.py
    Day1: added string manipulation examples (new line & concatenation)

--- Day-01/string_manipulation.py
+++ Day-01/string_manipulation.py
@@ -1,1 +1,17 @@
+# 1. New Line Escape Sequence
 
+# using `\n` to add a new line to strings.
+
+print("Hello")
+print("Word")
+
+# is the same as:
+
+print("Hello\nWorld")
+
+# 2. String Concatenation
+# Use the plus symbol `+` to concatenate different strings together
+
+print("My name is" + " " + "Shaheer")
+
+# will output: "My name is Shaheer"
