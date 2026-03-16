04d7c65c83400ead172cfb80386cb54d36550e1c
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-17 03:22:56 +0500

    Create 05_band_name_generator.py
    Band Name Generator – A simple Python program that helps users create a fun band name based on their city and a memorable word.

---
+++ Day-01/05_band_name_generator.py
@@ -0,0 +1,12 @@
+print("Welcome to Shaheer's 1st Python program!")
+print("To generate your band name, let me ask a few questions.\n")
+
+# Ask for city name
+city_name = input("What's the name of the city you grew up in?\n")
+
+# Ask for pet name
+pet_name = input("What is your pet's name?\n")
+
+# Generate band name
+band_name = city_name + " " + pet_name
+print("Your band name can be: " + band_name)
