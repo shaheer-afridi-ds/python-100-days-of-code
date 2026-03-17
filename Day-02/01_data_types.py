ffa3868432598b631096d31af8355ea7d7e47c2d
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-18 00:13:00 +0500

    Create 01_data_types.py
    This file demonstrates basic Python data types including strings, integers, floats, and booleans. It also shows simple operations like string concatenation and indexing.

---
+++ Day-02/01_data_types.py
@@ -0,0 +1,26 @@
+#There are 4 basic data types
+
+#strings
+print("123" + "456")
+#or
+print("hello"[0])
+print("hellow"[-1])
+
+#integers
+print(123 + 456)
+
+#floating point numbers or float
+print(3.149)
+
+#booleans
+print(True)
+print(False)
+
+# Subscripting
+print("Hello"[0])
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789)
