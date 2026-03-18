8ef7472eb469c7db84d5e61f47f2ff329976152d
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-19 00:08:33 +0500

    Create  01_if_else.py
    Added Day 03: if-else rollercoaster height check program

---
+++ Day-03/01_if_else.py
@@ -0,0 +1,8 @@
+print("Welcome to the roller coaster ride!\n")
+
+height = int(input("What's your height in cm? \n"))
+
+if height >= 50:
+    print("You can ride the rollercoaster! Here is your ticket ")
+else:
+    print("Sorry, you cannot ride the rollercoaster because your height is less than 50 cm.")
