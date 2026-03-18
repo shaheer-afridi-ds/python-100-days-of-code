c160f6ffde5c27ad07da004f1237ac3f39a81d94
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-19 00:10:20 +0500

    Create 02_even_odd_modulo.py
    Day 03: Implemented Even/Odd checker using modulo and if-else

---
+++ Day-03/02_even_odd_modulo.py
@@ -0,0 +1,8 @@
+print("Welcome to the Even/Odd Checker!\n")
+
+user_input = int(input("Enter a number to check if it's even or odd:\n"))
+
+if user_input % 2 == 0:
+    print("It's an even number!")
+else:
+    print("It's an odd number!")
