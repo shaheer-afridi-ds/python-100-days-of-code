104ada0fafd7d788dbfcac1d4a78d111a183f94a
Shaheer Afridi <shaheerafridi0@gmail.com>
2026-03-21 21:42:34 +0500

    Create highest_score.py
    Day 05 - Find highest score using loop and max function

---
+++ Day-05/highest_score.py
@@ -0,0 +1,11 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+print("The highest score in the list is", max_score)
+
+# Using built-in function
+print(max(student_scores))
